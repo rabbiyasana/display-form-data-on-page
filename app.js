1b3862612a95ddb484ae1d2dfa5d9537d002dac8
Rabbia sana <rabbiasana55@gmail.com>
2023-05-05 21:44:49 +0500

    get tages by clicking on add product button

---
+++ app.js
@@ -0,0 +1,7 @@
+// targeting tags by clicking on  add product button
+document.getElementById("btnAdd").addEventListener("click", ()=>{
+    const categories = document.getElementById("ddlCategories");
+    const productName = document.getElementById("txtName");
+    const productPrice = document.getElementById("txtPrice");
+    const gstPrice = document.getElementById("txtGstRate");
+})
