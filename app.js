3f59325166a24a17bdbd88a03a22f9db57bc80dc
Rabbia sana <rabbiasana55@gmail.com>
2023-05-05 22:13:28 +0500

    crate category class

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+// creating product category class
+class category{
+ constructor(id,name){
+   this.id = id;
+   this.name = name;
+ }
+ get id(){
+    return this._id;
+ }
+ set id(value){
+    if (value<1) {
+     console.log("id must be greater than 1");
+     return;
+    }
+    return this._id= value;
+ }
+
+ get name(){
+    return this._name;
+ }
+ set name(value){
+    if(value.length >= 3 && value.length<=50){
+        this._Name=value;            
+    }
+    else {
+        console.log("error")
+    }
+ }
+ get Summary(){
+    return `${this._id},${this._name}`;
+}
+}
+// creating product class
+class product{
+   constructor(id, name , price , gstRate, category){
+
+}
+}
+
 // targeting tags by clicking on  add product button
 document.getElementById("btnAdd").addEventListener("click", ()=>{
     const categories = document.getElementById("ddlCategories");
